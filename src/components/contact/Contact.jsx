d5ce17c11882bcae7006ab089c9c4772acd23b14
lukasz33 <lukasz.zacharzewski>
2022-11-21 16:46:28 +0100

    website update

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,7 +3,19 @@ import './contact.css'
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Porozmawiajmy</h5>
+      <h2>Napisz do mnie</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          
+        </div>
+        {/* END OF CONTACT OPTIONS */}
+        <form action=""></form>
+      </div>
+
+    </section>
   )
 }
 
